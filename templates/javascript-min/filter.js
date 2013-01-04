baa8f1628af4f03220e71b2d8745156f8c4f64a3
Yi <yiyin7@gmail.com>
2013-01-04 03:54:51 -0800

    adding minify safe formatting and optional parameters

--- templates/javascript-min/filter.js
+++ templates/javascript-min/filter.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('<%= grunt.util._.camelize(appname) %>App.filters')
-.filter('<%= _.camelize(name) %>', function() {
+angular.module('<%= grunt.util._.camelize(appname) %>App.filters', [])
+.filter('<%= _.camelize(name) %>', ['version', function(version) {
     return function(input) {
         return input;
     };
-});
+}]);
--- templates/javascript-min/service/service.js
+++ templates/javascript-min/service/service.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('<%= grunt.util._.camelize(appname) %>App.services')
-  .service('<%= _.camelize(name) %>', function <%= _.camelize(name) %>() {
+  .service('<%= _.camelize(name) %>', ['version', function <%= _.camelize(name) %>(version) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+  }]);
